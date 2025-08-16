64a59ad6963a94f1d5a15b49585ebabf997309d3
Jun <76056849+Junyoung316@users.noreply.github.com>
2025-08-16 19:43:03 +0900

    init

---
+++ src/main/java/com/reserve/illoomung/IlloomungApplication.java
@@ -0,0 +1,13 @@
+package com.reserve.illoomung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlloomungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlloomungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reserve/illoomung/IlloomungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserve.illoomung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlloomungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
