a78314f0481e11a0af85efa4a0a6e18945ace6e6
Jun <76056849+Junyoung316@users.noreply.github.com>
2025-11-30 17:54:33 +0900

    1

--- src/main/java/com/reserve/illoomung/presentation/business/ReserveInfoController.java
+++ src/main/java/com/reserve/illoomung/presentation/business/ReserveInfoController.java
@@ -12,11 +12,11 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class ReserveInfoController {
 
-    @GetMapping("/reserve/{storeId}")
-    public ResponseEntity<MainResponse<String>> searchStoreReserveInfo(@PathVariable("storeId") Long id) {
-        // id에 맞는 가게 예약 정보 조회
-        return ResponseEntity.ok(MainResponse.success());
-    }
+//    @GetMapping("/reserve/{storeId}")
+//    public ResponseEntity<MainResponse<String>> searchStoreReserveInfo(@PathVariable("storeId") Long id) {
+//        // id에 맞는 가게 예약 정보 조회
+//        return ResponseEntity.ok(MainResponse.success());
+//    }
 
     // 가게 예약 수정 삭제
 
